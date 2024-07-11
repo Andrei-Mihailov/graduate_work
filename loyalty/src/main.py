bff82f946e5632f43c1564815650ec33f9285d94
Pavel Ershov <40699895+PavelErsh@users.noreply.github.com>
2024-07-11 13:11:56 +0200

    Update main.py

--- loyalty/src/main.py
+++ loyalty/src/main.py
@@ -5,10 +5,56 @@ import string
 from datetime import datetime
 from pydantic import BaseModel, validator
 
-from models.models import Base, Promocode, PromoUsage
 from database import engine, get_db
 
 app = FastAPI()
+
+
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Date,
+    Boolean,
+    DECIMAL,
+    ForeignKey,
+    TIMESTAMP,
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class Promocode(Base):
+    __tablename__ = "promocodes"
+    id = Column(Integer, primary_key=True, index=True)
+    code = Column(String, unique=True, index=True, nullable=False)
+    discount_type = Column(String, nullable=False)  # "fixed", "percentage", "trial"
+    discount_value = Column(DECIMAL, nullable=False)  # сумма скидки или процент
+    expiration_date = Column(Date)
+    usage_limit = Column(
+        Integer
+    )  # ограничение на количество использований (NULL для неограниченного)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(TIMESTAMP, server_default="CURRENT_TIMESTAMP")
+
+
+class PromoUsage(Base):
+    __tablename__ = "promo_usage"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, nullable=False)
+    promocode_id = Column(Integer, ForeignKey("promocodes.id"), nullable=False)
+    used_at = Column(TIMESTAMP, server_default="CURRENT_TIMESTAMP")
+    is_successful = Column(Boolean, default=False)
+
+    promocode = relationship("Promocode", back_populates="usages")
+
+
+Promocode.usages = relationship(
+    "PromoUsage", order_by=PromoUsage.id, back_populates="promocode"
+)
+
 
 # Создаем таблицы в базе данных
 Base.metadata.create_all(bind=engine)
