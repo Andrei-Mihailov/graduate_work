0e3472c0b0d44dc29fc5b6072c41e32679bf75fe
Pavel Ershov <40699895+PavelErsh@users.noreply.github.com>
2024-07-21 16:07:16 +0200

    Update tests.py

--- admin/promocodes/tests.py
+++ admin/promocodes/tests.py
@@ -25,6 +25,24 @@ class PromoCodeModelTest(TestCase):
         self.assertIsNotNone(self.promo_code.creation_date)
         self.assertIsNotNone(self.promo_code.expiration_date)
 
+    def test_discount_type_choices(self):
+        valid_choices = [choice[0] for choice in PromoCode.DiscountType.choices]
+        self.assertIn(self.promo_code.discount_type, valid_choices)
+
+    def test_num_uses(self):
+        # Test default num_uses value
+        promo_code = PromoCode.objects.create(
+            code="TEST2025",
+            discount=5.0
+        )
+        self.assertEqual(promo_code.num_uses, 1)
+
+    def test_is_deleted(self):
+        self.assertFalse(self.promo_code.is_deleted)
+        self.promo_code.is_deleted = True
+        self.promo_code.save()
+        self.assertTrue(self.promo_code.is_deleted)
+
     def test_str_method(self):
         self.assertEqual(str(self.promo_code), "TEST2024")
 
@@ -77,12 +95,17 @@ class PurchaseModelTest(TestCase):
         self.assertEqual(self.purchase.total_price, 26.99)
         self.assertIsNotNone(self.purchase.purchase_date)
 
+    def test_default_purchase_date(self):
+        self.assertEqual(self.purchase.purchase_date.date(), timezone.now().date())
+
 
 class AvailableForUsersModelTest(TestCase):
 
     def setUp(self):
-        self.user = User.objects.create(username="testuser")
-        self.group = Group.objects.create(name="Test Group", description="A test group")
+        self.user1 = User.objects.create(username="testuser1")
+        self.user2 = User.objects.create(username="testuser2")
+        self.group1 = Group.objects.create(name="Test Group 1", description="A test group 1")
+        self.group2 = Group.objects.create(name="Test Group 2", description="A test group 2")
         self.promo_code = PromoCode.objects.create(
             code="TEST2024",
             discount=10.0,
@@ -91,13 +114,33 @@ class AvailableForUsersModelTest(TestCase):
             is_active=True
         )
         self.available = AvailableForUsers.objects.create(promo_code=self.promo_code)
-        self.available.user.set([self.user])
-        self.available.group.set([self.group])
+        self.available.user.set([self.user1, self.user2])
+        self.available.group.set([self.group1, self.group2])
 
     def test_available_for_users_creation(self):
-        self.assertIn(self.user, self.available.user.all())
-        self.assertIn(self.group, self.available.group.all())
+        self.assertIn(self.user1, self.available.user.all())
+        self.assertIn(self.user2, self.available.user.all())
+        self.assertIn(self.group1, self.available.group.all())
+        self.assertIn(self.group2, self.available.group.all())
         self.assertEqual(self.available.promo_code, self.promo_code)
+
+    def test_add_users_to_available_for_users(self):
+        new_user = User.objects.create(username="testuser3")
+        self.available.user.add(new_user)
+        self.assertIn(new_user, self.available.user.all())
+
+    def test_add_groups_to_available_for_users(self):
+        new_group = Group.objects.create(name="Test Group 3", description="A test group 3")
+        self.available.group.add(new_group)
+        self.assertIn(new_group, self.available.group.all())
+
+    def test_remove_user(self):
+        self.available.user.remove(self.user1)
+        self.assertNotIn(self.user1, self.available.user.all())
+
+    def test_remove_group(self):
+        self.available.group.remove(self.group1)
+        self.assertNotIn(self.group1, self.available.group.all())
 
     def test_str_method(self):
         # To test str method, you might want to implement a __str__ method in the model
