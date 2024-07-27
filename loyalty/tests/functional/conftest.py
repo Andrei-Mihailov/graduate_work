cfe2c5755047ab5d264b7361b3325264d4c49501
Andrei Mihailov <andrew19220@yandex.ru>
2024-07-27 17:14:42 +0300

    fix tests loyality

---
+++ loyalty/tests/functional/conftest.py
@@ -0,0 +1,37 @@
+import pytest_asyncio
+import aiohttp
+
+
+@pytest_asyncio.fixture()
+def make_post_request():
+    async def inner(url: str, param: dict = None, cookie=None):
+        session = aiohttp.ClientSession(cookies=cookie)
+        response = await session.post(url, params=param)
+        await session.close()
+        return response
+
+    return inner
+
+
+@pytest_asyncio.fixture()
+def make_put_request():
+    async def inner(url: str, param: dict = None, cookie=None):
+        session = aiohttp.ClientSession(cookies=cookie)
+
+        response = await session.put(url, params=param)
+        await session.close()
+        return response
+
+    return inner
+
+
+@pytest_asyncio.fixture()
+def make_get_request():
+    async def inner(url: str, cookie=None):
+        session = aiohttp.ClientSession(cookies=cookie)
+
+        response = await session.get(url)
+        await session.close()
+        return response
+
+    return inner
---
+++ loyalty/tests/functional/settings.py
@@ -0,0 +1,18 @@
+from os import environ as env
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+from pydantic import Field
+
+load_dotenv(".dev.env")
+
+
+class TestSettings(BaseSettings):
+    SERVICE_HOST: str = Field(default={env.get("SERVICE_HOST")})
+    SERVICE_PORT: int = Field(default={env.get("SERVICE_PORT")})
+
+    @property
+    def SERVIСE_URL(self):
+        return f"http://{self.SERVICE_HOST}:{self.SERVICE_PORT}"
+
+
+test_settings = TestSettings()
